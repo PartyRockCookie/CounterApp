1a9250974b44437cb377873d87181c643350e409
PartyRockCookie <wausrotas@gmail.com>
2016-03-14 20:08:25 -0400

    Added new boxes for numbers, working on arrows, and added new logic for timer

--- app/src/main/java/com/example/willie/sleeptimer/MainActivity.java
+++ app/src/main/java/com/example/willie/sleeptimer/MainActivity.java
@@ -9,12 +9,18 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnStart, btnStop;
-    static TextView textViewTime;
+    static TextView textViewTime, hourViewTime, minuteViewTime, secondViewTime;
+    EditText hourText, minuteText, secondText;
+    CounterClass timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +29,32 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Initializing all the variables
         btnStart =(Button)findViewById(R.id.btnStart);
         btnStop = (Button)findViewById(R.id.btnEnd);
         textViewTime = (TextView)findViewById(R.id.textViewTime);
+        hourViewTime = (TextView)findViewById(R.id.hoursTextView);
+        minuteViewTime = (TextView)findViewById(R.id.minutesTextView);
+        secondViewTime = (TextView)findViewById(R.id.secondsTextView);
+        hourText = (EditText)findViewById(R.id.hours);
+        minuteText = (EditText)findViewById(R.id.minutes);
+        secondText = (EditText)findViewById(R.id.seconds);
 
-        textViewTime.setText("00:00:00");
-       //textViewTime.setVisibility(View.INVISIBLE);
-        final CounterClass timer = new CounterClass(180000, 1000);
+        //Make timer invisible for now.
+        //textViewTime.setVisibility(View.INVISIBLE);
+
+
+
+
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               timer.start();
+                int hours = Integer.parseInt(hourText.getText().toString());
+                int minutes = Integer.parseInt(minuteText.getText().toString());
+                int seconds = Integer.parseInt(secondText.getText().toString());
+                long total = (TimeUnit.HOURS.toMillis(hours) + TimeUnit.MINUTES.toMillis(minutes) + TimeUnit.SECONDS.toMillis(seconds));
+                timer = new CounterClass(total, 1000);
+                timer.start();
             }
         });
 
